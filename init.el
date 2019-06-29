91ada1a2bca81d392f5795b542a98ac953431536
Samuel Cedarbaum <scedarbaum@gmail.com>
2019-06-28 23:37:48 -0700

    Initial commit

---
+++ init.el
@@ -0,0 +1,38 @@
+;;; init.el --- Entry point for Emacs configuration
+
+;;; Commentary:
+; This file does the following:
+; 1. Initializes the package manager
+; 2. Bootstraps use-package
+; 3. Ensures org-mode is installed
+; 4. Loads config.org (org-babel based configuration file)
+
+;;; Code:
+
+;; Initialize package manager
+(require 'package)
+
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+;; Bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Ensure org is installed in order to load config.org
+(use-package org
+  :ensure t
+  :defer t
+  :init
+  (setq org-confirm-babel-evaluate nil))
+
+;; Load config.org file
+(require 'org)
+(org-babel-load-file (expand-file-name (concat user-emacs-directory "config.org")))
+
+;;; init.el ends here
