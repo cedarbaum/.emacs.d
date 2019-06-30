28efb37903deeeb1ed36d6f3f9484e1174ed10e9
Samuel Cedarbaum <scedarbaum@gmail.com>
2019-06-29 17:23:50 -0700

    Align lists that use dotted pair notation.

--- init.el
+++ init.el
@@ -18,8 +18,8 @@
 ;; Initialize package manager
 (require 'package)
 
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("org"          . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa"        . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 
 (setq package-enable-at-startup nil)
