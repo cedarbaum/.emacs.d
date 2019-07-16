81f15c0d89c3f954eafbcf5bac6f977ecf5242a6
Sam Cedarbaum <scedarbaum@sams-air.lan>
2019-07-15 21:53:35 -0700

    Move package archive URLs to https.

--- init.el
+++ init.el
@@ -18,9 +18,9 @@
 ;; Initialize package manager
 (require 'package)
 
-(add-to-list 'package-archives '("org"          . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa"        . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("org"          . "https://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa"        . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 
 (setq package-enable-at-startup nil)
 (package-initialize)
