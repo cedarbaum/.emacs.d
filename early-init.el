b8dca4a25313a7385c7724d8ea1e7604c9061e37
Samuel Cedarbaum <scedarbaum@gmail.com>
2023-03-03 13:55:54 -0500

    Use `straight.el` for package management

---
+++ early-init.el
@@ -0,0 +1,11 @@
+;;; early-init.el --- Early Emacs configuration
+
+;;; Commentary:
+; This file executes early in the Emacs initialization process.
+
+;;; Code:
+
+;; Prevent package.el loading packages prior to their init-file loading
+(setq package-enable-at-startup nil)
+
+;;; early-init.el ends here
